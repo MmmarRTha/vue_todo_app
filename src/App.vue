9893924258885de1dbcb77c8c2024e7cc8587a26
MmmarRTHa <m27842n@outlook.com>
2025-04-09 20:47:16 -0700

    Initial commit for vue todo app

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="max-w-3xl mx-auto my-4">
+        <h1 class="text-gray-900 dark:text-white font-bold text-5xl text-center">
+            Hello Vue
+        </h1>
+    </main>
+</template>
