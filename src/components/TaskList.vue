116ef7ef37094c88140a46f6dee6999474518b3d
MmmarRTHa <m27842n@outlook.com>
2025-04-10 22:59:02 -0700

    - update: feature/list_rendering - enhance task item layout with flexbox for better responsiveness

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -11,15 +11,15 @@ const emits = defineEmits<{
 </script>
 
 <template>
-    <article v-for="task in props.tasks" :key="task.id" class="bg-gray-800 text-white p-5 rounded-md mt-6">
-        <label>
-            <input
-            @input="emits('toggleDone', task.id)" 
-            :checked="task.done"
-            type="checkbox" 
-            class="mr-2" 
-            >
-            <span :class="{'line-through text-gray-400': task.done}">{{ task.title }}</span>
+    <article v-for="task in props.tasks" :key="task.id"
+        class="bg-gray-800 text-white p-5 rounded-md mt-6 flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <label class="flex items-center">
+            <input @input="emits('toggleDone', task.id)" :checked="task.done" type="checkbox" class="mr-2">
+            <span :class="{ 'line-through text-gray-400': task.done }">{{ task.title }}</span>
         </label>
+        <button
+            class="bg-cyan-600 hover:bg-cyan-700 text-white text-base font-medium py-2 px-4 rounded-sm transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2 w-full mt-2 sm:mt-0 sm:w-auto">
+            Remove
+        </button>
     </article>
-</template> 
+</template>
